c1f7c5708a37c8c321c60855defd8fa83545c07f
smiles7v <45764852+smiles7v@users.noreply.github.com>
2020-06-20 12:52:00 +0100

    aggregatedpipeline

--- dslscripts/scripts.groovy
+++ dslscripts/scripts.groovy
@@ -58,4 +58,11 @@ listView('List View DSLs') {
         buildButton()
     }
 }
-
+deliveryPipelineView('job-dsl delivery pipeline') {
+    showAggregatedPipeline true
+    enableManualTriggers true
+    pipelineInstances 5
+    pipelines {
+        component('job-dsl delivery pipeline', 'job-dsl-checkout')
+    }
+}
